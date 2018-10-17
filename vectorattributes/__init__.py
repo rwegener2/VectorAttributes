ec77ebfa08e2e78747afba551900a029a7bd9af1
rwegener2 <rachel.e.wegener@gmail.com>
2018-10-17 15:44:39 -0600

    still updating projection

--- vectorattributes/__init__.py
+++ vectorattributes/__init__.py
@@ -2,7 +2,6 @@ from .dsmcalc import *
 from .dsmfootprint import *
 from .footprint import *
 from .geojson_check import *
-from .projection import *
 
-__all__ = ['dsmcalc', 'dsmfootprint', 'footprint', 'geojson_check', 'projection']
+__all__ = ['dsmcalc', 'dsmfootprint', 'footprint', 'geojson_check']
 __name__ = 'vectorattributes'
